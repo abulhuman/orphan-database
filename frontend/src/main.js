ac2cb2c9a165dda4a8352e41ec36ad535a538116
AD-em <adem164416@gmail.com>
2022-01-23 18:32:54 +0300

    FIX: vue environment variables setup to load properly

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,8 +7,8 @@ import vuetify from './plugins/vuetify';
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === 'production'
-    ? `${process.env.HOSTNAME}/`
+  process.env.VUE_APP_NODE_ENV === 'production'
+    ? `${process.env.VUE_APP_HOSTNAME}/`
     : 'http://localhost:3000/';
 axios.defaults.withCredentials = true;
 
